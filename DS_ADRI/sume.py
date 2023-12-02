550e159c37fd90d839beafb26e7c0658f28dabb1
ChuyMendoza333 <mo_si_mo@hotmail.com>
2023-12-02 03:59:31 -0600

    new

---
+++ DS_ADRI/sume.py
@@ -0,0 +1,9 @@
+print('*'*55)
+print('''         SUME CALCULATOR''')
+print('*'*55)
+print('''\nThis is a program to make a sume of two numbers!!!
+please enter only numeric values:\n''')
+num1 = input('enter a numer:\n')
+num2 = input('enter other number:\n')
+print('='*55)
+print(f'Sume of {num1} + {num2} is:\n{num1+num2}' )
